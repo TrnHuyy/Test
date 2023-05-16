071fdc3d9afec31c9934aa742a6533aa43693a19
TrnHuyy <trnhuyworkspace@gmail.com>
2023-05-16 10:04:10 +0700

    test

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System;
+using System.IO;
+
+namespace CD
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var drives = DriveInfo.GetDrives();
+            //DriveInfo drive = new DriveInfo("C");
+
+            foreach(var drive in drives)
+            {
+                Console.WriteLine($"{drive.Name}");
+                Console.WriteLine($"{drive.DriveType}");
+                Console.WriteLine($"{drive.VolumeLabel}");
+                Console.WriteLine($"{drive.DriveFormat}");
+                Console.WriteLine($"{drive.TotalSize}");
+                Console.WriteLine($"{drive.TotalFreeSpace}");
+            }
+            
+            
+        }
+    }
+}
